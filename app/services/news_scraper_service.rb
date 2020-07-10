fa149910b3eb17579a396d131b84a0c28fd9ea52
msimon42 <msimon@uab.edu>
2020-07-10 17:02:32 -0600

    Add request articles method

--- app/services/news_scraper_service.rb
+++ app/services/news_scraper_service.rb
@@ -1,4 +1,13 @@
 class NewsScraperService
+  def request_articles(form_data)
+    json_form_data = jsonify_form_data(form_data)
+
+    request = connection.post do |req|
+      req.body = json_form_data
+    end
+
+    JSON.parse(request)
+  end
 
   private
     def connection
@@ -7,7 +16,16 @@ class NewsScraperService
       end
     end
 
-    def articles_request_endpoint
-      'https://www.news-scraper.com/api/request-articles'
+    def jsonify_form_data(form_data)
+      {
+        startDate: form_data['start_date'],
+        endDate: form_data['end_date'],
+        keywords: form_data['keywords'],
+        amount: form_data['amount']
+      }.to_json
+    end
+
+    def news_scraper_uri
+      'https://www.news-scraper.com'
     end
 end
