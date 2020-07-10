cd470ce34556d78b18f87a9b8bf6701f5be3e447
msimon42 <msimon@uab.edu>
2020-07-10 16:36:25 -0600

    Add connection

--- app/services/news_scraper_service.rb
+++ app/services/news_scraper_service.rb
@@ -1,6 +1,12 @@
 class NewsScraperService
 
   private
+    def connection
+      Faraday.new(articles_request_endpoint) do |f|
+        f.adapter Faraday.default_adapter
+      end
+    end
+
     def articles_request_endpoint
       'https://www.news-scraper.com/api/request-articles'
     end
