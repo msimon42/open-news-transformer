d78ab558b5e5233baa696596f5411f8228b969c1
msimon42 <msimon@uab.edu>
2020-07-27 22:47:27 -0600

    Add

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
 class ArticlesController < ApplicationController
-  def request
-  
+  def request_form; end
+
+  def get_articles
+    @articles = NewsScraperService.new.request_articles(request_articles_params)
   end
 
+  private
+    def request_articles_params
+      params.permit(:startDate, :endDate, :keywords, :amount)
+    end
 end
