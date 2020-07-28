68d9b9334320e2362b1934129a7a5427c8d94d2f
msimon42 <msimon@uab.edu>
2020-07-28 14:57:58 -0600

    Add route for scrape form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'application#index'
 
   get '/articles-request', to: 'articles#request_form'
+  get '/articles-scrape', to: 'articles#scrape_form'
   get '/articles-results', to: 'articles#get_articles'
 end
