38c0acaa30f4a32245eee21fef0a7c62bbc11077
msimon42 <msimon@uab.edu>
2020-07-28 18:31:19 -0600

    Add connection

--- app/services/inferkit_service.rb
+++ app/services/inferkit_service.rb
@@ -1,7 +1,13 @@
 class InferkitService
 
   private
-    def endpoint
+    def connection
+      Faraday.new(url: inferkit_endpoint) do |f|
+        f.adapter Faraday.default_adapter
+      end
+    end
+
+    def inferkit_endpoint
       'https://api.inferkit.com/v1/models/standard/generate'
-    end  
+    end
 end
